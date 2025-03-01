cfebae8a09e1801199308a17ed3383d66e428fa8
Matt Keefer <mrkeefer8@gmail.com>
2025-03-01 17:02:58 -0500

    Add clickable actions to simple list

--- src/Components/Lists/SimpleList.tsx
+++ src/Components/Lists/SimpleList.tsx
@@ -1,20 +1,27 @@
 import "./index.css";
 
-export default function SimpleList({listItems}: {
-  listItems: { name: string, image?: string }[]
+export default function SimpleList({listItems, clickable}: {
+  listItems: { name: string, image?: string, onClick?: () => void }[],
+  clickable?: boolean,
 }) {
   return (
       <ul className={listItems.length > 7 ?
           "list-group list-group-flush list-unstyled simple-list" :
           "list-group list-group-flush list-unstyled"}>
-        {listItems.map((item, i) => <li
-            className="list-group-item concert-info-list-item rounded mb-2"
-            key={item.name + i}>
-          <div className="rounded-5 d-flex justify-content-between align-items-center">
-            {item.name}
-            <img src={item.image} alt={item.name} className="rounded-5 concert-img-icon"/>
-          </div>
-        </li>)}
+        {listItems.map((item, i) => (
+            <li
+                className={clickable ?
+                    "list-group-item concert-info-list-item rounded mb-2 clickable-list-item" :
+                    "list-group-item concert-info-list-item rounded mb-2"}
+                onClick={item.onClick}
+                key={item.name + i}>
+              <div className="rounded-5 d-flex justify-content-between align-items-center">
+                {item.name}
+                {item.image &&
+                    <img src={item.image} alt={item.name} className="rounded-5 concert-img-icon"/>}
+              </div>
+            </li>
+        ))}
       </ul>
   );
 }
